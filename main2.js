48122a82baaef457908df06bb2ca52ba6e528360
eliotbo <eliot.bolduc@gmail.com>
2022-05-26 01:43:29 -0400

    added codemirror

---
+++ main2.js
@@ -0,0 +1,52 @@
+
+
+import init, { greet, do_parse } from "./target/glsl2wgsl.js";
+
+var editor1 = CodeMirror.fromTextArea(document.getElementById("code"), {
+    lineNumbers: true,
+    // theme: "dracula",
+    // lineWrapping: true,
+    // indentUnit: 4,
+    // mode: "text/rust",
+
+    matchBrackets: true,
+});
+
+editor1.setSize("500", "500");
+
+var editor2 = CodeMirror.fromTextArea(document.getElementById("code2"), {
+    lineNumbers: true,
+    // theme: "dracula",
+    // lineWrapping: true,
+    // indentUnit: 4,
+    // mode: "text/rust",
+
+    matchBrackets: true,
+});
+
+editor2.setSize("500", "1000");
+
+init()
+    .then(() => {
+        greet("WebAssembly")
+
+
+
+    });
+
+window.dum = function dum() {
+
+
+
+
+
+    // let t = document.getElementById("glslarea");
+    let t = editor1.getValue();
+    // // let tt = t.innerHTML.valueOf();
+    // let tt = t.value;
+    let savior = do_parse(t);
+
+    editor2.setValue(savior);
+
+    // document.getElementById("code").innerHTML = savior;
+}
