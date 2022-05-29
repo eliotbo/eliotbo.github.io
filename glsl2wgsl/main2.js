59ca572c9e39b3881d19b2b9c6a49466d852cb1c
eliotbo <eliot.bolduc@gmail.com>
2022-05-29 15:02:05 -0400

    deleted ALL codemirror-example dependencies

--- glsl2wgsl/main2.js
+++ glsl2wgsl/main2.js
@@ -2,43 +2,43 @@
 
 import init, { greet, do_parse } from "./glsl2wgsl.js";
 // import CodeMirror from "../dist/codemirror-examples.js";
-{/* <script src="../dist/codemirror-examples.js"></script> */ }
+/* <script src="../dist/codemirror-examples.js"></script> */
 
 // const CodeMirror = require('../dist/codemirror-examples.js');
 
-// var editor1 = CodeMirror.fromTextArea(document.getElementById("code"), {
-//     lineNumbers: true,
-//     // theme: "dracula",
-//     // lineWrapping: true,
-//     // indentUnit: 4,
-//     // mode: "text/rust",
-//     // mode: "text/x-csrc",
+var editor1 = CodeMirror.fromTextArea(document.getElementById("code"), {
+    lineNumbers: true,
+    theme: "dracula",
+    // lineWrapping: true,
+    // indentUnit: 4,
+    // mode: "text/rust",
+    // mode: "text/x-csrc",
 
-//     matchBrackets: true,
-// });
+    matchBrackets: true,
+});
 
-// editor1.setSize(null, "1000");
+editor1.setSize(null, "1000");
 
-// var editor2 = CodeMirror.fromTextArea(document.getElementById("code2"), {
-//     lineNumbers: true,
-//     // theme: "dracula",
-//     // lineWrapping: true,
-//     // indentUnit: 4,
-//     // mode: "rust",
+var editor2 = CodeMirror.fromTextArea(document.getElementById("code2"), {
+    lineNumbers: true,
+    theme: "dracula",
+    // lineWrapping: true,
+    // indentUnit: 4,
+    // mode: "rust",
 
-//     matchBrackets: true,
-// });
+    matchBrackets: true,
+});
 
-// editor2.setSize(null, "1000");
+editor2.setSize(null, "1000");
 
-var css = $('textarea[mode="text/x-c"]');
-var html = $('textarea[mode="rust"]');
+// var css = $('textarea[mode="text/x-c"]');
+// var html = $('textarea[mode="rust"]');
 
-var editor1 = css.editor();
-editor1.setSize(null, "800");
+// var editor1 = css.editor();
+// editor1.setSize(null, "800");
 
-var editor2 = html.editor();
-editor2.setSize(null, "800");
+// var editor2 = html.editor();
+// editor2.setSize(null, "800");
 
 init()
     .then(() => {
